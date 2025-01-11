956204c3abfdc8719a93a6ca9a6276d47cfab09a
Rei Tsukada <rayzou1120@icloud.com>
2025-01-11 12:29:46 +0900

    simple grep

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::process;
 use std::path;
 use clap::Parser;
 
@@ -8,6 +7,14 @@ struct Cli {
     path: path::PathBuf
 }
 
+/// executed by cargo run --bin <pattern> <filename>
 fn main() {
     let args = Cli::parse();
+
+    let content = std::fs::read_to_string(&args.path).expect("cound not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
